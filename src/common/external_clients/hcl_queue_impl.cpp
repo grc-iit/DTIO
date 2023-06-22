d5e6b90adffd70f6052e04f4604f8a16bc961e38
nrj5k <10393055+nrj5k@users.noreply.github.com>
2023-06-22 12:50:26 -0500

    checkpoint: use nats guidelines for queues

--- src/common/external_clients/hcl_queue_impl.cpp
+++ src/common/external_clients/hcl_queue_impl.cpp
@@ -25,40 +25,63 @@
 int
 HCLQueueImpl::publish_task (task *task_t)
 {
-  key = std::to_string (name) + KEY_SEPARATOR + key;
-  auto true_key = HCLKeyType (key);
-  if (hcl_client->Push(true_key, value)
-    {
-      return 0;
-    }
-  else
-    {
-      std::cerr << "put failed for key:" << key << "\n";
-      return 1;
-    }
+  auto msg = serialization_manager ().serialize_task (task_t);
+  // TODO: replace with hcl queue
+  // natsConnection_PublishString (nc, subject.c_str (), msg.c_str ());
+  return 0;
 }
 
 task *
 HCLQueueImpl::subscribe_task_with_timeout (int &status)
 {
+  // TODO: replace with HCL
+  // natsMsg *msg = nullptr;
+  // natsSubscription_NextMsg (&msg, sub, MAX_TASK_TIMER_MS);
+  if (msg == nullptr)
+    return nullptr;
+  task *task
+      = serialization_manager ().deserialize_task (natsMsg_GetData (msg));
+  status = 0;
+  return task;
 }
 
 task *
 HCLQueueImpl::subscribe_task (int &status)
 {
+  // TODO: replace with HCL
+  // natsMsg *msg = nullptr;
+  // natsSubscription_NextMsg (&msg, sub, MAX_TASK_TIMER_MS_MAX);
+  if (msg == nullptr)
+    return nullptr;
+  task *task
+      = serialization_manager ().deserialize_task (natsMsg_GetData (msg));
+  status = 0;
+  return task;
 }
 
 int
 HCLQueueImpl::get_queue_size ()
 {
+  // TODO: replace with HCL
+  int size_of_queue;
+  natsSubscription_GetPending (sub, nullptr, &size_of_queue);
+  return size_of_queue;
 }
 
 int
 HCLQueueImpl::get_queue_count ()
 {
+  // TODO: replace with HCL
+  // int *count_of_queue = new int ();
+  // natsSubscription_GetStats (sub, count_of_queue, NULL, NULL, NULL, NULL,
+  //                           NULL);
+  int queue_count = *count_of_queue;
+  delete (count_of_queue);
+  return queue_count;
 }
 
 int
 HCLQueueImpl::get_queue_count_limit ()
 {
+  return INT_MAX;
 }
